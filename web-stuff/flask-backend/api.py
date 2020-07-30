861ca048b52c1cd5a08a5b8860591f00277668c2
Keith <keithbsmith87@gmail.com>
2020-07-30 15:17:00 -0400

    s

--- web-stuff/flask-backend/api.py
+++ web-stuff/flask-backend/api.py
@@ -343,7 +343,7 @@ def cpu_use():
 def mem_use():
     return Response(str(psutil.virtual_memory().percent))
 
-@app.route('/net_use', method=['GET'])
+@app.route('/net_use', methods=['GET'])
 def net_use():
     return Response(str(psutil.net_if_stats()['wlan0'].speed))
 
